a16fccd48343f75791174d569e7e10eae59ce619
Jimmy Byrd <jimmy.byrd@motivity.net>
2025-09-05 10:35:51 -0400

    Refactor FSharpTask execution logic to use cancellable tasks

--- SomeMsbuildTask.fsx
+++ SomeMsbuildTask.fsx
@@ -1,14 +1,19 @@
 namespace SomeMsbuildTask
 
 #r "nuget: Microsoft.Build.Utilities.Core"
+#r "nuget: IcedTasks"
+
+
+#nowarn "0988" // warning FS0988: Main module of program is empty: nothing will happen when it is run. This is expected for MSBuild tasks.
 
 open Microsoft.Build.Framework
-
-
-#nowarn "0988" // warning FS0988: Main module of program is empty: nothing will happen when it is run
+open IcedTasks
+open System
 
 type FSharpTask() =
     inherit Microsoft.Build.Utilities.Task()
+
+    member private x.CancellationTokenSource = new System.Threading.CancellationTokenSource()
 
     [<Required>]
     member val Property1 = "" with get, set
@@ -16,17 +21,23 @@ type FSharpTask() =
     [<Output>]
     member val Result = "" with get, set
 
-    override this.Execute() =
-        try
-            // Example logic for the task
-            if this.Property1 = "" then
-                this.Log.LogError("Property1 must be set.")
-                false
-            else
-                this.Log.LogWarning($"Property1: {this.Property1}, Property2: {this.Property2}")
-                this.Result <- $"Task executed successfully. Property1: {this.Property1}, Property2: {this.Property2}"
-                true
-        with
-        | ex ->
-            this.Log.LogErrorFromException(ex)
-            false
+    member inline private x.ExecuteCore = cancellableTask {
+
+        // Example logic for the task
+        if String.IsNullOrWhiteSpace x.Property1 then
+            x.Log.LogError("Property1 must be not null or whitespace.")
+            return false
+        else
+            if x.Property1.Contains "Error" then
+                failwith "Simulated failure for demonstration purposes."
+
+            // Using warning log to demonstrate logging
+            x.Log.LogWarning($"Property1: {x.Property1}, Property2: {x.Property2}")
+            x.Result <- $"Task executed successfully. Property1: {x.Property1}, Property2: {x.Property2}"
+            return true
+    }
+
+    override x.Execute() = x.ExecuteCore(x.CancellationTokenSource.Token).GetAwaiter().GetResult()
+
+    interface ICancelableTask with
+        member x.Cancel() = x.CancellationTokenSource.Cancel()
